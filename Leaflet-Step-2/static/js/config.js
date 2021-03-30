259efae6183b10bcae203c47fd64ea28ec218347
kate <kate_spitzer@yahoo.com>
2021-03-29 23:42:33 -0700

    adding new layers to step 2

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,2 +1,3 @@
 //// API key
 const API_KEY = "pk.eyJ1Ijoia21zcGl0emVyIiwiYSI6ImNrbWh5ZWxpcDBieGEydnFtaGs4dnVwdDkifQ.EzHLQoqR_8K8DO8r202HZA";
+const OMNI_API_KEY = "ucsdmap-d18c8fe5";
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -63,31 +63,39 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "mapbox/satellite-v9",
     accessToken: API_KEY
   });
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var grayscalemap = L.tileLayer("https://maps.omniscale.net/v2/{accessToken}/style.grayscale/{z}/{x}/{y}.png", {
+    attribution: '&copy; 2021 &middot; <a href="https://maps.omniscale.com/">Omniscale</a> ' +
+    '&middot; Map data: <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    accessToken: OMNI_API_KEY
+  });
+
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "dark-v10",
+    id: "outdoors-v11",
     accessToken: API_KEY
   });
 
   // define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Satellite": satellitemap,
+    "Grayscale": grayscalemap,
+    "Outdoors": outdoormap
   };
 
   // create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes
+    "Earthquakes": earthquakes
   };
 
   // create our map, giving it the streetmap and earthquakes layers to display on load
@@ -96,7 +104,7 @@ function createMap(earthquakes) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [streetmap, earthquakes]
+    layers: [satellitemap, earthquakes]
   });
 
   // Create a layer control
